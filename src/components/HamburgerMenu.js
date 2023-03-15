b0cfb068761771c9a8065f3f7400f825efd172f2
amirthenew <ah.abshari@hotmail.com>
2023-03-15 23:57:01 +0330

    ham menu coding

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -2,7 +2,17 @@ import React, { useState } from 'react';
 import styles from './HamburgerMenu.module.css'
 
 const HamburgerMenu = () => {
+
+    const[isOpen,setItOpen]=useState(false)
+    const handleClick = ()=>{
+        setItOpen(isOpen)
+    }
+
     return ( <div className={styles.container}>
+
+        <div className={styles.hamicon} onClick={handleClick}>
+        <div></div>
+        </div>
 
         </div> );
 }
