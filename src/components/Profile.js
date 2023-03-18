39fcdecb236d62b4a56d54bf382e7bbc7c45ae62
amirthenew <ah.abshari@hotmail.com>
2023-03-18 23:57:59 +0330

    styling photo

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,19 +2,21 @@ import React from 'react';
 import styles from './Profile.module.css'
 const Profile = () => {
     return (
+
+        
         <div className={styles.container}>
-
-        <div className={styles.textbox}>
-        <h2>Who I Am ?!</h2>
-        <p>
-       I'm Amirhossein Abshari A junior front-end web developer who loves Javascript and now i'm happy cause I've launched my first react full static web page 
-        </p>
-        <div className={styles.photo}>
-        </div>
-        </div>
-
+        <div className={styles.photo}>asdassd</div>
+          <div className={styles.textbox}>
+            <h2>Who I Am ?!</h2>
+            <p>
+             I'm Amirhossein Abshari A junior front-end web developer who loves Javascript and now i'm happy cause I've launched my first react full static web page 
+            </p>
 
         </div>
+        </div>
+
+
+      
       )
 }
  
