905b07a4259ad60891b7291ef8a89629d2c88f32
amirthenew <ah.abshari@hotmail.com>
2023-03-19 23:26:37 +0330

    clg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,11 +8,9 @@ const Navbar = ()=>{
     const[isOpen,setItOpen]=useState(false)
     const handleClick = ()=>{
         setItOpen(!isOpen)
-
-
     }
 
-
+console.log(isOpen);
 
 return(<div className={styles.maindiv}>
     <header className={styles.header}>
@@ -32,7 +30,8 @@ return(<div className={styles.maindiv}>
     <div className={styles.container}>
         {isOpen && (    
 <div className={styles.menubox}>
-<ul className={`${isOpen ? console.log('dropdown') : console.log('dropup')}`}>
+<ul className={`${isOpen ? styles.dropdownmenu : styles.dropupmenu}`}>
+
   <Link onClick={handleClick} className={styles.Link} to='/'><li className={styles.hamLi}>Home</li></Link>
     <Link onClick={handleClick} className={styles.Link} to='/products'><li className={styles.hamLi}>Products</li></Link>
    <Link onClick={handleClick} className={styles.Link} to='/aboutus'><li className={styles.hamLi}>About Us</li></Link>
