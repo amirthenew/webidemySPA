25d75857c321de4fc848fad639e6417f8c150c68
amirthenew <ah.abshari@hotmail.com>
2023-03-21 23:54:26 +0330

    fix hamburger menu bug

--- src/components/HamburgerIcon.js
+++ src/components/HamburgerIcon.js
@@ -6,9 +6,9 @@ const HamburgerIcon = (props) => {
 const {isOpen,handleClick} = props
     return (
         <div className={styles.hamicon} onClick={handleClick} >
-        <div className={`${isOpen ? styles.line1close : styles.line1}`}></div>
-        <div className={`${isOpen ? styles.line2close : styles.line2}`}></div>
-        <div className={`${isOpen ? styles.line3close : styles.line3}`}></div>
+        <div className={`${!isOpen ? styles.line1close : styles.line1}`}></div>
+        <div className={`${!isOpen ? styles.line2close : styles.line2}`}></div>
+        <div className={`${!isOpen ? styles.line3close : styles.line3}`}></div>
     </div>  );
 }
  
