ee4fdd791762efef626345e0db1e8d59062690da
amirthenew <ah.abshari@hotmail.com>
2023-04-30 00:14:43 +0430

    destructure props

--- src/components/HamburgerMenuOpen.js
+++ src/components/HamburgerMenuOpen.js
@@ -3,15 +3,15 @@ import styles from './HamburgerMenuOpen.module.css';
 import { Link } from 'react-router-dom';
 
 const HamburgerMenuOpen = (props) => {
-
+const handleClick = props
     return ( 
 <div className={styles.container}>
 <div className={styles.menubox}>
 <ul className={styles.dropdownmenu}>
-<Link onClick={props.handleClick} className={styles.Link} to='/'><li className={styles.hamLi}>Home</li></Link>
-<Link onClick={props.handleClick} className={styles.Link} to='/products'><li className={styles.hamLi}>Products</li></Link>
-<Link onClick={props.handleClick} className={styles.Link} to='/aboutus'><li className={styles.hamLi}>About Us</li></Link>
-<Link onClick={props.handleClick} className={styles.Link} to='/'><li className={styles.hamLi}>Contact Us</li></Link>
+<Link onClick={handleClick} className={styles.Link} to='/'><li className={styles.hamLi}>Home</li></Link>
+<Link onClick={handleClick} className={styles.Link} to='/products'><li className={styles.hamLi}>Products</li></Link>
+<Link onClick={handleClick} className={styles.Link} to='/aboutus'><li className={styles.hamLi}>About Us</li></Link>
+<Link onClick={handleClick} className={styles.Link} to='/'><li className={styles.hamLi}>Contact Us</li></Link>
 </ul>
 
 {console.log('dropdown')}
