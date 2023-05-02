20c243f1cfbbb16622f9fd5fc6cceb29b9699c85
amirthenew <ah.abshari@hotmail.com>
2023-05-03 01:22:11 +0430

    styling contactus

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -5,28 +5,66 @@ import styled from 'styled-components';
 const ContactUs = () => {
 
 
-    const StyledContainer = styled.div`
-    width : 100px;
-    background-color : #3f3;
-    height : 100px;
-    color : #f43;
+
+    const Container = styled.div`
+   width:100%;
+   min-height:100vh;
+   background-color :#505050;
+    
     `
+
     const StyledHeading = styled.h1`
-    color : #3f2;
+    color : #000;
     font-size : 2rem;
+    text-align : center;
+    padding-top : 1rem;
+    
+    `
+    const Maindiv = styled.div`
+    width : 90%;
+    display : flex;
+    margin : auto ;
+    `
+
+    const Socialbox = styled.div`
+    width : 40%;
+    background-color : #435;
+    height : 500px;
+
+    border-radius : 1rem;
+    
+    `
+
+    const ContactBox = styled.div`
+    width : 60%;
+    background-color : #343;
+    height : 500px;
+    
     `
 
 
     return ( 
+
  
-    
-  <StyledContainer>
-   <StyledHeading>
-   contactus
-   </StyledHeading>
-   </StyledContainer>
+      <Container>
+      <StyledHeading>
+Contact Us
+  </StyledHeading>
+
+  <Maindiv>
+   <Socialbox>
+   My github
+   </Socialbox>
+
+   <ContactBox>
+   <input type='text'/>
+   </ContactBox>
+  </Maindiv>
 
 
+
+
+  </Container>
  
   );
 }
