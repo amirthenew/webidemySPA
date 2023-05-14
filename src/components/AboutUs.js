652cabe9c0c12856315e089906e78bb44c4a007a
amirthenew <ah.abshari@hotmail.com>
2023-05-15 00:49:33 +0430

    create right side and left side

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import styles from './AboutUs.module.css'
 
 
 
 const AboutUs = ()=>{
 
-    return <div>
+    return (<div>
     
     <h1>About us</h1>
 
+    <div className={styles.container}>
+
+    <div className={styles.leftside}>
+    
     </div>
+    <div className={styles.rightside}>
+    
+    </div>
+    </div>
+
+    </div>)
 }
 
 
