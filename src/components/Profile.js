3361b63f7a3f6d4a2bbe1f9ade0762d6da7ddd15
amirthenew <ah.abshari@hotmail.com>
2023-05-17 02:20:42 +0430

    styling profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Profile.module.css'
+import { Link } from 'react-router-dom';
 const Profile = () => {
     return (
 
@@ -9,8 +10,12 @@ const Profile = () => {
           <div className={styles.textbox}>
             <h3>Who I Am ?!</h3>
             <p>
-             I'm Amirhossein Abshari A junior front-end web developer who loves Javascript and now i'm happy cause I've launched my first react full static web page 
+             I'm Amirhossein Abshari A junior front-end web developer who loves Javascript and now i'm happy cause I've launched my first react full static web page ... <span className={styles.moreinfo}> <Link to='/aboutus'>more info</Link></span>
             </p>
+
+            
+
+          
 
         </div>
         </div>
