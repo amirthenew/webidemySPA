99ca1a64929b60864d8658776ac0dde1e737f48a
amirthenew <ah.abshari@hotmail.com>
2023-05-22 00:49:22 +0430

    add image src to blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,11 +1,12 @@
 import React,{useState} from 'react';
 import BlogBox from './BlogBox';
 import styles from './Blog.module.css'
+import reactImg from '../images/React.png'
 
 const Blog = () => {
 
   const[blog,setBlog]=useState([
-  {id:1,date:'10 march 2023',title:'whats react',desc : ' its a long story i fell in website design 4 years ago I had a background in this profession and it was a reason that Ive been in this route for 4 years I was thinking on launch a startup in'},
+  {id:1,date:'10 march 2023',title:'whats react',desc : ' its a long story i fell in website design 4 years ago I had a background in this profession and it was a reason that Ive been in this route for 4 years I was thinking on launch a startup in',src:reactImg},
   {id:2,date:'1 apr 2023',title:'whats Javascript',desc : ' lorem ipsum is lorem ipsum lorem ipsum'}
 ,{id:3,date:'1 may 2023',title:'whats Html Css',desc : ' lorem ipsum is lorem ipsum lorem ipsum'}])
     return (
@@ -13,12 +14,13 @@ const Blog = () => {
       <div className={styles.maindiv}>
 
       <div className={styles.container}>
-      <h2>Webidemy blog</h2>
+      <h2>Webilog</h2>
       <p>
       This blog is the official source for the updates from the React team. Anything important, including release notes or deprecation notices, will be posted here first. You can also follow the @reactjs account on Twitter, but you won’t miss anything essential if you only read this blog.
       </p>
 {blog.map(blog=>
-  <BlogBox         
+  <BlogBox  
+  src={blog.src}       
   id={blog.id} 
   date={blog.date}
   title={blog.title} 
--- src/components/BlogBox.js
+++ src/components/BlogBox.js
@@ -6,14 +6,17 @@ import { Link } from 'react-router-dom';
 const BlogBox = (props) => {
 
     
-const {id,title,desc,date} = props
+const {id,title,desc,date,src} = props
 
 
     return ( 
 <div><div className={styles.blogcontainer}>
         <h2>{id}</h2>
+        <img style={{width:'50px'}} src={src}/>
         <h2>{title}</h2>
-        <spam>{date}</spam>
+        <span>{date}</span>
+        
+
         <p>
        {desc}
         </p>
